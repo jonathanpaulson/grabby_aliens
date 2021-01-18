06f7fb678fc6e099fa6c27c6e5f0dd8ac32e2dd8
Jonathan Paulson <jonathanpaulson@gmail.com>
2021-01-17 19:38:36 -0500

    Allow multiple comma-separated n values

--- analyze.py
+++ analyze.py
@@ -10,7 +10,7 @@ import math
 
 parser = argparse.ArgumentParser(description='Run Grabby Aliens model')
 parser.add_argument('--D', type=int, help='Dimensions of space')
-parser.add_argument('--n', type=float, help='Origin time power-law')
+parser.add_argument('--n', type=str, help='Origin time power-law; can pass multiple comma-separated values')
 parser.add_argument('--N', type=str, help='Number of potential civilizations')
 parser.add_argument('--c', type=float, default=1.0, help='The speed of light')
 parser.add_argument('--L', type=float, default=1.0, help='The size of the universe')
@@ -20,69 +20,80 @@ parser.add_argument('--empty_samples', type=int, default=0, help='How many point
 
 args = parser.parse_args()
 D,n,N,s,c,L,seed,empty_samples = args.D,args.n,int(float(args.N)),args.s,args.c,args.L,args.seed,args.empty_samples
-if s != 1.0:
-    print('WARNING: s!=1.0 so graph titles are misleading')
+ns = [float(n) for n in n.split(',')]
 
-fname = f'D={D}_n={n}_N={N:.2e}_L={L}_c={c}_seed={seed}'
-subprocess.check_output(f'g++ -std=c++17 -O3 -Wall -Werror -Wextra -Wshadow -Wno-sign-compare simulate.cc && ./a.out {D} {n} {N} {s} {c} {L} {fname} {seed} {empty_samples}', shell=True)
+DATA = {}
+for n in ns:
+    fname = f'D={D}_n={n}_N={N:.2e}_L={L}_c={c}_seed={seed}'
+    subprocess.check_output(f'g++ -std=c++17 -O3 -Wall -Werror -Wextra -Wshadow -Wno-sign-compare simulate.cc && ./a.out {D} {n} {N} {s} {c} {L} {fname} {seed} {empty_samples}', shell=True)
+    print(f'Generated data in {fname}.csv and {fname}_years.csv')
 
-print(f'Generated data in {fname}.csv and {fname}_years.csv')
+    # Read CIV data
+    with open(f'{fname}.csv') as csvfile:
+        CIVS = list(csv.DictReader(csvfile))
 
-# Read CIV data
-CIVS = []
-with open(f'{fname}.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        CIVS.append(row)
+    # Read years data
+    with open(f'{fname}_years.csv') as yearfile:
+        YEARS = list(csv.DictReader(yearfile))
 
-# Read years data
-YEARS = []
-with open(f'{fname}_years.csv') as yearfile:
-    reader = csv.DictReader(yearfile)
-    for row in reader:
-        YEARS.append(row)
+    DATA[n] = (CIVS, YEARS)
 
-# Rescale model times so median(Origin)=1.0
-T50 = np.median([float(row['OriginTime']) for row in CIVS])
-TS = [float(row['OriginTime'])/T50 for row in CIVS]
-WS = [float(row['MinArrival'])/T50 for row in CIVS]
-SS = [float(row['MinSee'])/T50 for row in CIVS]
-
+C = '.'.join([str(len(CIVS)) for (CIVS,YEARS) in DATA.values()])
 # Make graphs
 fig, p = plt.subplots(4,2,constrained_layout=True,figsize=(12,12))
-fig.suptitle(f'D={D} n={n} N={N:.2e} L={L} |C|={len(TS)} |C|/L^D={len(TS)/L**D}')
+fig.suptitle(f'D={D} n={n} N={N:.2e} L={L} c={c} s={s} |C|={C}')
 
-def plot(ax, x, y, ylabel, log):
+def plot(ax, label, log):
     ax.minorticks_on()
     ax.grid(b=True, which='major', axis='both')
     ax.tick_params(axis='both', which='both', bottom=True, left=True)
     ax.set_xlabel('Percentile')
-    ax.plot(x, y)
-    ax.set_ylabel(ylabel)
+    for n,(CIVS,YEARS) in DATA.items():
+        civs_x = [float(i)/len(CIVS) for i in range(len(CIVS))]
+        years_x = [float(i)/len(YEARS) for i in range(len(YEARS))]
+        # Rescale model times so median(Origin)=1.0
+        T50 = np.median([float(row['OriginTime']) for row in CIVS])
+        if label == 'Origin':
+            x = civs_x
+            y = [float(row['OriginTime'])/T50 for row in CIVS]
+        elif label == 'Origin (Gyr)':
+            x = years_x
+            y = sorted([float(row['OriginTime']) for row in YEARS])
+        elif label == 'MinArrival':
+            x = civs_x
+            y = sorted([float(row['MinArrival'])/T50 for row in CIVS])
+        elif label == 'MinWait (Gyr)':
+            x = years_x
+            y = sorted([float(row['MinWait']) for row in YEARS])
+        elif label == 'MinSee':
+            x = civs_x
+            y = sorted([float(row['MinSee'])/T50 for row in CIVS])
+        elif label == 'MinSETI (Gyr)':
+            x = years_x
+            y = sorted([float(row['MinSETI']) for row in YEARS])
+        elif label == 'MaxAngle':
+            x = civs_x
+            y = sorted([float(row['MaxAngle']) for row in CIVS])
+        elif label == '% Empty':
+            x = civs_x
+            y = list(reversed(sorted([float(row['PctEmpty']) for row in CIVS])))
+        else:
+            assert False, f'Unknown label={label}'
+        ax.plot(x, y, label=f'n={n}')
+    ax.legend(loc='lower right');
+    ax.set_ylabel(label)
     if log:
         ax.set_yscale('log')
 
-civs_x = [float(i)/len(CIVS) for i in range(len(CIVS))]
-years_x = [float(i)/len(YEARS) for i in range(len(YEARS))]
-
-plot(p[0,0], civs_x, TS, 'Origin', log=False)
-origin_years = sorted([float(row['OriginTime']) for row in YEARS])
-plot(p[0,1], years_x, origin_years, 'Origin (Gyr)', log=True)
-
-plot(p[1,0], civs_x, sorted(WS), 'MinArrival', log=False)
-wait_years = sorted([float(row['MinWait']) for row in YEARS])
-plot(p[1,1], years_x, wait_years, 'MinWait (Gyr)', log=True)
-
-plot(p[2,0], civs_x, sorted(SS), 'MinSee', log=False)
-
-seti_years = sorted([float(row['MinSETI']) for row in YEARS])
-plot(p[2,1], years_x, seti_years, 'MinSETI (Gyr)', log=True)
-
-angles = sorted([float(row['MaxAngle']) for row in CIVS])
-plot(p[3,0], civs_x, angles, 'MaxAngle', log=False)
-E = [float(row['PctEmpty']) for row in CIVS]
-if any([e for e in E]): # if we generated PctEmpty data
-    plot(p[3,1], civs_x, list(reversed(sorted(E))), '% Empty', log=False)
+plot(p[0,0], 'Origin', log=False)
+plot(p[0,1], 'Origin (Gyr)', log=True)
+plot(p[1,0], 'MinArrival', log=False)
+plot(p[1,1], 'MinWait (Gyr)', log=True)
+plot(p[2,0], 'MinSee', log=False)
+plot(p[2,1], 'MinSETI (Gyr)', log=True)
+plot(p[3,0], 'MaxAngle', log=False)
+if empty_samples:
+    plot(p[3,1], '% Empty', log=False)
 else:
     fig.delaxes(p[3,1])
 
